85b58de8ec754245f4e480c63db71cb01c9904da
Junnama Noda <junnama@alfasado.jp>
2016-01-18 15:46:47 +0900

    first commit.

---
+++ plugins/ThemeSystemField/lib/ThemeSystemField/L10N/ja.pm
@@ -0,0 +1,12 @@
+package ThemeSystemField::L10N::ja;
+use strict;
+use base 'ThemeSystemField::L10N';
+
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'It includes Custom Fields of system scope to Theme.' => 'システムスコープのカスタムフィールドをテーマに含めます。',
+    'Custom Fields(system)' => 'カスタムフィールド(システム)',
+    );
+
+1;
